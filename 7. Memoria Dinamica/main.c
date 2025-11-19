a80e66f4ac6f7cdcd2c1814d32d9fa79fe7090b7
Edoardo Oranger <edo.oranger@gmail.com>
2025-11-19 21:27:09 +0100

    feat: Aggiornamento Capitolo
    Completata descrizione dell'allocazione dinamica della memoria

---
+++ 7. Memoria Dinamica/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main (const int argc, const char** argv)
+{
+    uint8_t first_array[6]      = { 1, 2, 3, 4, 5, '\0' };
+    char*   string              = "my string";
+    char    another_string[]    = { 'a', 'b', 'c', 'd', '\0' };
+
+    return 0;
+}
