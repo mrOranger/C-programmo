5ef954d9e95f3a2f7e60a34f55f35cec8329d1b2
Edoardo Oranger <edo.oranger@gmail.com>
2026-02-08 11:52:28 +0100

    feat: Definizione Prototipi
    Create definizione dei prototipi per funzione 'concat'

---
+++ string/src/concat.c
@@ -0,0 +1,12 @@
+#include "string.h"
+#include "string.c"
+
+string_t *concat_char(string_t *input_string, char *sequence_to_concat)
+{
+    return NULL;
+}
+
+string_t *concat_string(string_t *input_string, string_t *string_to_concat)
+{
+    return NULL;
+}
